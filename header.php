28caed1a305518c6405846363130aafd4c68a964
Qadash Ban <qadash@users.noreply.github.com>
2015-05-29 04:00:03 -0400

    Create header.php

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Header</title>
+	</head>
+	<body>
+	<?php
+	print 'Here is the header section'
+	
+	?>
+	
+	
+
+
+
+</body>
+</html>
